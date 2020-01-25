85a4efe3bf302cd71bc61642c8cf348d16666677
aditya vikram parakala <44452865+aditya-vikram-parakala@users.noreply.github.com>
2020-01-24 20:41:37 -0500

    Auth

---
+++ AuthDemo/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+app.set("view engine","ejs");
+ 
+app.get("/", function(req,res){
+res.render("home");
+});
+
+app.get("/secret", function(req,res){
+    res.render("secret");
+});
+app.listen(3001, function(req,res){
+    console.log("Server started!!");
+});
